75cfaba0139eb5a150bf5faf4a87de681fdca778
hi-artem <aakatev@virtru.com>
2022-11-01 11:34:10 -0700

    update info

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 ## Why do you need this?
 
-OpenSSL is [about to announce a major bug affecting 3.x version](https://mta.openssl.org/pipermail/openssl-announce/2022-October/000238.html).
+OpenSSL has announced [a major bug affecting 3.x version](https://mta.openssl.org/pipermail/openssl-announce/2022-October/000238.html), known as SpookySSL. Lookup CVE-2022-3786 and CVE-2022-3602 for further details.
 
 Unfortunately for Palo Alto Networks Prisma Cloud Compute users, this tool doesn't have a way to search all images by a package name. 
 
 This script will search all your deployed containers, and dump them in a single json.
+
+The script is targeting affected version of `openssl`, and binaries known to be compiled with vulnerable `openssl` version, like nodejs. See [this document](https://github.com/NCSC-NL/OpenSSL-2022/tree/main/software) for more information on affected software.
 
 ## How to run this?
 
--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 'use strict';
 
 const fs = require('fs');
-let { getAllImages} = require('./images');
+let { getAllImages } = require('./images');
 
 let main = async () => {
   let images = await getAllImages()
   let out = []
-  
+
   images.forEach(image => {
     image['packages'].forEach(packageClass => {
       packageClass['pkgs'].forEach(pkg => {
-        if(pkg.name.includes('openssl') && pkg.version.startsWith('3')) {
+        if (pkg.name.includes('openssl') && pkg.version.startsWith('3')) {
+          out.push(image)
+        }
+        else if (pkg.name.includes('nodejs') && (
+          pkg.version.startsWith('17')
+          || pkg.version.startsWith('18')
+          || pkg.version.startsWith('19'))) {
           out.push(image)
         }
       })
     })
   });
-  
-  
+
+
   fs.writeFile('report.json', JSON.stringify(out), function (err) {
     if (err) return console.log(err);
     console.log('report generated!');
